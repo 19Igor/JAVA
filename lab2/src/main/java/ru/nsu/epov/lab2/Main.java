ee02986cdfa90280dc3230ff799a0648425e940b
Igor Epov <i.epov@g.nsu.ru>
2022-04-04 14:36:09 +0700

    add maven

--- lab2/src/main/java/ru/nsu/epov/lab2/Main.java
+++ lab2/src/main/java/ru/nsu/epov/lab2/Main.java
@@ -13,10 +13,15 @@ import ru.nsu.epov.lab2.reading.toReadStrings;
 
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.logging.Logger;
 
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) throws IOException, InvocationTargetException, NoSuchMethodException {
+
+
 
         final int FileMode = 0;
         final int WorkFlowMode = 1;
---
+++ lab2/src/test/java/ru/nsu/epov/lab2/OperationFabric/DivisionTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.epov.lab2.OperationFabric;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivisionTest {
+
+    @org.junit.jupiter.api.Test
+    void command() {
+
+        // Here directly tests are written.
+    }
+}
